e38dffd41e61251a4d5d02239a7b00187777b1e6
shnooki <vikisahputra17@gmail.com>
2022-12-28 23:22:04 +0700

    conf

--- shared/infrastructure/server/http_server_gin.go
+++ shared/infrastructure/server/http_server_gin.go
@@ -24,7 +24,7 @@ func NewGinHTTPHandler(log logger.Logger, address string, appData gogen.Applicat
 	_, err := os.LookupEnv("PORT")
 
 	if err {
-		router.Run(":" + os.Getenv("PORT"))
+		router.Run()
 	}
 
 	// PING API
