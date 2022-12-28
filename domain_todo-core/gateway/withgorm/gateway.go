70388a43a34685bae660242d36d8b42b585eaaa7
shnooki <vikisahputra17@gmail.com>
2022-12-29 00:29:11 +0700

    solvedBug

--- domain_todo-core/gateway/withgorm/gateway.go
+++ domain_todo-core/gateway/withgorm/gateway.go
@@ -79,7 +79,7 @@ func (r *Gateway) GetAllTodo(ctx context.Context, page int, size int) ([]*entity
 	var todo []*entity.Todo
 
 	var count int64
-
+	flag := false
 	if err := r.Db.
 		Model(entity.Todo{}).
 		Count(&count).
@@ -87,9 +87,12 @@ func (r *Gateway) GetAllTodo(ctx context.Context, page int, size int) ([]*entity
 		Find(&todo); err.RowsAffected == 0 {
 		return nil, count, page, errorenum.DataNull
 	}
-	// if int64(size) > count || int64(size) != count {
-	// 	count = int64(size)
-	// }
+	if int64(size) > count && int64(size) != count {
+		flag = true
+	}
+	if !flag {
+		count = int64(size)
+	}
 	return todo, count, page, nil
 }
 
